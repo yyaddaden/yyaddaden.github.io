518a343a699a2a5ac57cf2b9b3c5075a0b20b374
Yacine YADDADEN <yacine.yaddaden@gmail.com>
2018-04-01 16:07:09 -0400

    Some minor modifications

--- js/script.js
+++ js/script.js
@@ -1,17 +1,17 @@
 $(document).ready(function() {
-$('#fullpage').fullpage({
-	anchors:["link-about", "link-work", "link-publications", "link-education"],
-		navigation: true, controlArrows: false, slidesNavigation: true, slidesNavPosition: 'bottom', scrollOverflow: true,})
+	$('#fullpage').fullpage({
+		anchors:["link-about", "link-work", "link-publications", "link-education"],
+		navigation: true, controlArrows: false, slidesNavigation: true, slidesNavPosition: 'bottom', scrollOverflow: true,
+	})
 
 	$( ".btn-menu" ).click(function() {
-
-	$('.ui.sidebar').sidebar('toggle');
+		$('.ui.sidebar').sidebar('toggle');
 	});
 
 	$('.ui.sidebar').sidebar('setting', 'transition', 'overlay');
 
-$('.special.cards .image').dimmer({
-  on: 'hover'
-});
+	$('.special.cards .image').dimmer({
+	 	on: 'hover'
+	});
 });
 
