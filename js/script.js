0252a1ac9f91c688d1efb0da09fc9653b562ad63
Yacine YADDADEN <yacine.yaddaden@gmail.com>
2018-10-30 11:09:46 -0400

    update - add active state in the navigation

--- js/script.js
+++ js/script.js
@@ -1,7 +1,16 @@
 $(document).ready(function() {
 	$('#fullpage').fullpage({
 		anchors:["link-about", "link-work", "link-publications", "link-education"],
-		navigation: true, controlArrows: false, slidesNavigation: true, slidesNavPosition: 'bottom', scrollOverflow: true,
+		navigation: true, 
+		controlArrows: false, 
+		slidesNavigation: true, 
+		slidesNavPosition: 'bottom', 
+		scrollOverflow: true,
+		onLeave: function(idx_orig, idx_dest){
+			var anchors = ["link-about", "link-work", "link-publications", "link-education"];
+			$('#it_' + anchors[idx_orig - 1]).attr('class', "item");
+			$('#it_' + anchors[idx_dest - 1]).attr('class', "item active");
+		}
 	})
 
 	$( ".btn-menu" ).click(function() {
